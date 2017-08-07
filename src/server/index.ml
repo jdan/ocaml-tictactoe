29227628a2c38febc0357341ec9f244aa3bb3185
Jordan Scales <scalesjordan@gmail.com>
2017-08-07 12:04:56 -0400

    let's roll

---
+++ src/server/index.ml
@@ -0,0 +1,19 @@
+type req
+
+type res
+external send : res -> string -> unit = "send" [@@bs.send]
+
+type server
+external get : server -> string -> (req -> res -> unit) -> unit = "get" [@@bs.send]
+external post : server -> string -> (req -> res -> unit) -> unit = "post" [@@bs.send]
+
+external listen : server -> int -> unit = "listen" [@@bs.send]
+external express : unit -> server = "express" [@@bs.module]
+
+let app = express ();;
+let port = 3000;;
+
+get app "/" (fun _ -> fun res -> send res "Hello, world!");;
+
+listen app port;;
+Js.log ("Server listening on port " ^ string_of_int(port) ^ "...")
